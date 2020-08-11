879b56e1beab869eee6d8b64cc6084b9ef5206c8
Chris Blank <Christoph.Blank@kumavision.de>
2020-08-11 19:25:28 +0200

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,1 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ChrisBlankDe/Crap/master/vmss_ext.ps1'))
